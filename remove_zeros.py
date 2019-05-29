f60928cd77f4910ea4d67eee53e7ed1cd3770ae6
rev997 <me15btech11027@iith.ac.in>
2019-05-29 18:52:16 +0530

    Update remove_zeros.py

--- remove_zeros.py
+++ remove_zeros.py
@@ -1,36 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 import numpy as np
 
-
-# In[2]:
-
-
 original=np.load('dataset.npy')
-
-
-# In[3]:
-
-
 final=[]
-
-
-# In[4]:
-
-
 for values in original:
     if values[1]>0:
         final.append([values[0],values[1]])
     if values[1]<0:
         final.append([values[0],values[1]])
-
-
-# In[5]:
-
-
 np.save('final_no_zeros.npy',final)
-
